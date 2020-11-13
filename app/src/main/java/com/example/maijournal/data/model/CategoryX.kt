db90de6662d008d5e9ed743f5ec3956d76da0dff
Loveth Nwokike <chidi@Loveths-MacBook-Pro.local>
2020-11-13 16:46:30 +0100

    add categories view

--- app/src/main/java/com/example/maijournal/data/model/CategoryX.kt
+++ app/src/main/java/com/example/maijournal/data/model/CategoryX.kt
@@ -6,10 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "category")
 data class CategoryX(
-
     @PrimaryKey
     @ColumnInfo(name = "name")
     val name: String,
     @ColumnInfo(name="icon")
     val icon: String
-)
+){
+    override fun toString(): String {
+        return "CategoryX(name='$name', icon='$icon')"
+    }
+}
---
+++ app/src/main/java/com/example/maijournal/utils/CategoryAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.maijournal.utils
+
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.maijournal.R
+import com.example.maijournal.data.model.CategoryX
+
+
+class CategoryAdapter( mcontext: Context,
+    val categories: List<CategoryX>)
+    : ArrayAdapter<CategoryX>(mcontext, 0, categories) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var dropdownItemView = convertView
+
+        if (dropdownItemView == null)
+            dropdownItemView = LayoutInflater.from(context)
+                .inflate(R.layout.item_layout, parent,false)
+
+        val category = categories[position]
+
+        val flagImage: ImageView = dropdownItemView!!.findViewById(R.id.categoryIv)
+        val res = context.resources.getIdentifier(category.icon,"drawable",context.packageName)
+        Log.d("im", "getView: $res")
+        flagImage.setImageResource(res)
+
+        val langTextView: TextView = dropdownItemView.findViewById(R.id.categoryTv)
+        langTextView.text = category.name
+
+        return dropdownItemView
+    }
+
+
+}
